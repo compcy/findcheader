2d3e6de732aebc8164dd64ddea2d48c081dfcfb5
mmyers <mmyers@agleader.com>
2020-08-20 11:34:19 -0500

    Fix remaining Pylint warnings.

--- findcheader.py
+++ findcheader.py
@@ -22,21 +22,21 @@ def find_headers(filename: str) -> [str]:
     return found_headers
 
 
-def location_in_file(filename: str, header: str) -> (int, int):
+def location_in_file(filename: str, header_str: str) -> (int, int):
     """Determines the usage location of a specified header in a file."""
     with open(filename) as infile:
         for (line_num, line) in enumerate(infile.readlines()):
-            loc = line.find(header)
+            loc = line.find(header_str)
             if loc >= 0:
                 return line_num + 1, loc
     return None
 
 
-def print_header_warning(header: str, warning: str):
+def print_header_warning(filename: str, header_str: str, warning: str):
     """Print a warning line for a header usage"""
-    (line_num, location) = location_in_file(filename, header)
+    (line_num, location) = location_in_file(filename, header_str)
     print(r"{filename}:{line}:{location} warning: {warning} '{header}'".format(
-        filename=filename, line=line_num, location=location, warning=warning, header=header))
+        filename=filename, line=line_num, location=location, warning=warning, header=header_str))
 
 
 if __name__ == "__main__":
@@ -44,15 +44,15 @@ if __name__ == "__main__":
         print(r"Error, no input file.")
         sys.exit()
 
-    filename = sys.argv[1]
+    input_file = sys.argv[1]
 
-    if not os.path.isfile(filename):
+    if not os.path.isfile(input_file):
         sys.exit()
 
     standard = []
     suspicious = []
 
-    headers = find_headers(filename)
+    headers = find_headers(input_file)
     for header in headers:
         if header in std_headers:
             standard.append(header)
@@ -61,8 +61,8 @@ if __name__ == "__main__":
 
     if standard:
         for header in standard:
-            print_header_warning(header, r"Use of C-header")
+            print_header_warning(input_file, header, r"Use of C-header")
 
     if suspicious:
         for header in suspicious:
-            print_header_warning(header, r"Use of suspicious header (might be C)")
+            print_header_warning(input_file, header, r"Use of suspicious header (might be C)")
