d6a7fddcbdb3062d4d10b5e55940d3be525918bf
mmyers <mmyers@agleader.com>
2020-08-20 11:28:03 -0500

    Add if __main__

--- findcheader.py
+++ findcheader.py
@@ -5,15 +5,6 @@ import os
 import re
 import sys
 
-if len(sys.argv) < 2:
-    print(r"Error, no input file.")
-    sys.exit()
-
-filename = sys.argv[1]
-
-if not os.path.isfile(filename):
-    sys.exit()
-
 std_headers = [r"<assert.h>", r"<complex.h>", r"ctype.h", r"<errno.h>",
                r"<fenv.h>", r"<float.h>", r"<inttypes.h>", r"<iso646.h>",
                r"<limits.h>", r"<locale.h>", r"<math.h>", r"<setjmp.h>",
@@ -22,9 +13,6 @@ std_headers = [r"<assert.h>", r"<complex.h>", r"ctype.h", r"<errno.h>",
                r"<stdlib.h>", r"<stdnoreturn.h>", r"<string.h>", r"<tgmath.h>",
                r"<threads.h>", r"<time.h>", r"<uchar.h>", r"<wchar.h>",
                r"<wctype.h>"]
-
-standard = []
-suspicious = []
 
 
 def location_in_file(filename: str, header: str) -> (int, int):
@@ -37,15 +25,6 @@ def location_in_file(filename: str, header: str) -> (int, int):
     return None
 
 
-with open(filename, 'r') as infile:
-    headers = re.findall(r"<.*\.h>", infile.read())
-    for header in headers:
-        if header in std_headers:
-            standard.append(header)
-        else:
-            suspicious.append(header)
-
-
 def print_header_warning(header: str, warning: str):
     """Print a warning line for a header usage"""
     (line_num, location) = location_in_file(filename, header)
@@ -53,10 +32,31 @@ def print_header_warning(header: str, warning: str):
         filename=filename, line=line_num, location=location, warning=warning, header=header))
 
 
-if standard:
-    for header in standard:
-        print_header_warning(header, r"Use of C-header")
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print(r"Error, no input file.")
+        sys.exit()
 
-if suspicious:
-    for header in suspicious:
-        print_header_warning(header, r"Use of suspicious header (might be C)")
+    filename = sys.argv[1]
+
+    if not os.path.isfile(filename):
+        sys.exit()
+
+    standard = []
+    suspicious = []
+
+    with open(filename, 'r') as infile:
+        headers = re.findall(r"<.*\.h>", infile.read())
+        for header in headers:
+            if header in std_headers:
+                standard.append(header)
+            else:
+                suspicious.append(header)
+
+    if standard:
+        for header in standard:
+            print_header_warning(header, r"Use of C-header")
+
+    if suspicious:
+        for header in suspicious:
+            print_header_warning(header, r"Use of suspicious header (might be C)")
